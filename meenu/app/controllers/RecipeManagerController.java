5bc60c35b0d4df6e40789ed578a567472cd2d69a
Leif H. Larsen <me@leiflarsen.org>
2016-02-21 15:45:53 +0100

    Added UI for recipes
    Shell implementation

--- meenu/app/controllers/RecipeManagerController.java
+++ meenu/app/controllers/RecipeManagerController.java
@@ -8,7 +8,15 @@ import views.html.*;
 public class RecipeManagerController extends Controller {
 
     public Result index() {
-        return ok(recipemanager.render("This is the page for recipe planning... :D:D"));
+        return ok(recipemanager.render());
     }
+	
+	public Result addRecipe() {
+		return ok(recipeadd.render());
+	}
+	
+	public Result editRecipe(Long recipeId) {
+		return ok(recipeedit.render(Long.toString(recipeId)));
+	}
 
 }
