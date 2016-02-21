1ec683e3f086467b06b72d26bcb6f5e995bf1d85
Leif H. Larsen <me@leiflarsen.org>
2016-02-21 16:05:16 +0100

    UI for people
    Shell implementation

--- meenu/app/controllers/PeopleController.java
+++ meenu/app/controllers/PeopleController.java
@@ -10,5 +10,13 @@ public class PeopleController extends Controller {
     public Result index() {
         return ok(people.render("This is the page for controlling people... So they can do evil shit... MOHAHAHAHA"));
     }
+	
+	public Result addPerson() {
+		return ok(peopleadd.render());
+	}
+	
+	public Result editPerson(Long personId) {
+		return ok(peopleedit.render(Long.toString(personId)));
+	}
 
 }
