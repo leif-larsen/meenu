f89481f0ce71f20ab7a5f7cffa6fa3bf86ce4b69
Leif H. Larsen <me@leiflarsen.org>
2016-02-21 15:27:22 +0100

    Product stuff
    Shell implementations of product add/edit and product type add/edit

--- meenu/app/controllers/ProductManagerController.java
+++ meenu/app/controllers/ProductManagerController.java
@@ -10,5 +10,16 @@ public class ProductManagerController extends Controller {
     public Result index() {
         return ok(productmanager.render("This is the page for managing products... :D:D"));
     }
-
+	
+	public Result editProductType(Long typeId) {
+		return ok(producttypeedit.render(Long.toString(typeId)));
+	}
+	
+	public Result addProductType() {
+		return ok(addproducttype.render());
+	}
+	
+	public Result editProduct(Long productId) {
+		return ok(productedit.render(Long.toString(productId)));
+	}
 }
